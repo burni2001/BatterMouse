5e1d99b6191f06f1a14153bb274dba537d7b814e
Bernhard Oettlin <bernhard@mueller.oettlin.com>
2026-03-13 16:39:41 +0100

    chore: update assembly info with latest commit hash
    Updated assembly information files to reflect the latest commit hash (70ad1e56d7a6f88e7187251a769f6227d569112d) across both BatterMouse and BatterMouse.Tests projects. This ensures proper version tracking and build consistency.

--- BatterMouse/obj/Debug/net8.0-windows10.0.19041.0/win-x64/BatterMouse.AssemblyInfo.cs
+++ BatterMouse/obj/Debug/net8.0-windows10.0.19041.0/win-x64/BatterMouse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BatterMouse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+133a35e3f53349de7b57234fe6d0483f18b293f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70ad1e56d7a6f88e7187251a769f6227d569112d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BatterMouse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BatterMouse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
