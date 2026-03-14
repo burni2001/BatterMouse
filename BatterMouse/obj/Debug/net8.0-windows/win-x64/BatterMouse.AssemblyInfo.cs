9760c4d04d90bd9436bcb20b0e20b30c064a16c2
Bernhard Oettlin <bernhard@mueller.oettlin.com>
2026-03-14 20:08:44 +0100

    chore: update assembly info and cache files for latest commit
    Updated assembly information and cache files to reflect the latest commit hash c803b2f575d0df68b81d6532003fb956be75e3c2, ensuring build artifacts are synchronized with the current codebase state.

--- BatterMouse/obj/Debug/net8.0-windows/win-x64/BatterMouse.AssemblyInfo.cs
+++ BatterMouse/obj/Debug/net8.0-windows/win-x64/BatterMouse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BatterMouse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+519105e6f50c3547322da1b8078aeee931d764a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c803b2f575d0df68b81d6532003fb956be75e3c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BatterMouse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BatterMouse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
