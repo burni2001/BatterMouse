8330edbb740493176f378d58bd84b3b192785015
Bernhard Oettlin <bernhard@mueller.oettlin.com>
2026-03-20 02:35:38 +0100

    chore(deps): update .NET SDK to 10.0.201
    Update .NET SDK from version 10.0.200 to 10.0.201 across all projects, including BatterMouse and BatterMouse.Tests. This update includes the latest SDK improvements and security patches.

--- BatterMouse.Tests/obj/Debug/net8.0-windows10.0.19041.0/BatterMouse.Tests.AssemblyInfo.cs
+++ BatterMouse.Tests/obj/Debug/net8.0-windows10.0.19041.0/BatterMouse.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BatterMouse.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f015dbfe23c24a838eb3a6ee840208bbe0e158a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+281cfeb5e2865a88b0d44da2f0d767a4961bb151")]
 [assembly: System.Reflection.AssemblyProductAttribute("BatterMouse.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BatterMouse.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BatterMouse/obj/Debug/net8.0-windows/win-x64/BatterMouse.AssemblyInfo.cs
+++ BatterMouse/obj/Debug/net8.0-windows/win-x64/BatterMouse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BatterMouse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b0b7dadcc94ef852aaaaaf28b846e58900a4ef3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+281cfeb5e2865a88b0d44da2f0d767a4961bb151")]
 [assembly: System.Reflection.AssemblyProductAttribute("BatterMouse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BatterMouse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
